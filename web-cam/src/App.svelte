b008c0e0be90933e9f979748580a3c957b25214e
Eliot Horowitz <eliothorowitz@gmail.com>
2025-11-29 19:44:23 -0500

    auto reload

--- web-cam/src/App.svelte
+++ web-cam/src/App.svelte
@@ -1,5 +1,14 @@
 <script lang="ts">
  import Start from "./start.svelte";
+ import { onMount } from "svelte";
+
+ onMount(() => {
+   const interval = setInterval(() => {
+     location.reload();
+   }, 10 * 60 * 1000); // 10 minutes
+
+   return () => clearInterval(interval);
+ });
 </script>
 
 <main>
